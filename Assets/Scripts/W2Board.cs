65b34f4fae7c8a8df0cdc9012d21b984aeeec1b0
Wilmer420 <wrodriguez420@yahoo.com>
2020-02-13 18:37:06 -0500

    Added a class folder
    Wilmer

---
+++ Assets/Scripts/W2Board.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class W2Board : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A)) //left//
+        {
+            player.transform.position += new Vector3(0, 0, 1);
+
+        }
+        if (Input.GetKeyDown(KeyCode.D)) //right//
+        {
+            player.transform.position += new Vector3(0, 0, -1);
+
+        }
+        //if (Input.GetKeyDown(KeyCode.Q)) //up//
+        {
+            //player.transform.position += new Vector3(0, 1, 0);
+
+        }
+        //if (Input.GetKeyDown(KeyCode.E)) //down//
+        {
+           //player.transform.position += new Vector3(0, -1, 0);
+
+        }
+        if (Input.GetKeyDown(KeyCode.W)) //forward//
+        {
+            player.transform.position += new Vector3(1, 0, 0);
+
+        }
+        if (Input.GetKeyDown(KeyCode.S)) //backwards//
+        {
+            player.transform.position += new Vector3(-1, 0, 0);
+
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            player.transform.position = new Vector3(0,1,0);
+
+        }
+    }
+}
